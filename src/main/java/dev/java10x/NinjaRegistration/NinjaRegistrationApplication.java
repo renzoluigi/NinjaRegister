bc1740020cf32f8ae3c570f37988194b9d2192f3
renzo <renzooluigi@hotmail.com>
2025-03-11 22:22:16 -0300

    first commit

---
+++ src/main/java/dev/java10x/NinjaRegistration/NinjaRegistrationApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.NinjaRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/NinjaRegistration/NinjaRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.NinjaRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
